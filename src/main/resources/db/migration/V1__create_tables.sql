e839cf17f88d0e0212a9621670375579ad504f5d
Ana Julia Gurita <ana.gurita@estudante.ufla.br>
2026-05-30 03:54:29 -0300

    Fix: Altera coluna de senha para VARCHAR(255) na migracao do Flyway

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE pessoa (
     cpf CHAR(11) NOT NULL,
     cep CHAR(9) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL
+    senha VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE livro (
